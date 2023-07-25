4f63034be2e8731cd7e4251b31e2486229f30096
Andrew <papapin777@gmail.com>
2023-07-25 11:18:28 -0700

    update test

--- tests/findTwoNumbers.spec.js
+++ tests/findTwoNumbers.spec.js
@@ -11,3 +11,16 @@ test('should return correct output', () => {
 	const result = findTwoNumbers([1, 2, 3, 4, 5, 6], 11);
 	expect(result).toEqual(expectedOutput);
 });
+
+test('should return correct pair of numbers', () => {
+	const value = 11;
+	const result = findTwoNumbers([1, 2, 3, 4, 5, 6], value);
+	expect(result[0] + result[1]).toBe(value);
+});
+
+test('should return an empty array if no pair found', () => {
+	const value = 11000;
+	const expectedOutput = [];
+	const result = findTwoNumbers([1, 2, 3, 4, 5, 6], value);
+	expect(result).toStrictEqual(expectedOutput);
+});
